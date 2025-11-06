cebe197f16e2032a9fe0fe4c14b2c01c1326d62c
longwatermelon <kt@yami.so>
2025-11-05 19:11:23 -0500

    set

---
+++ lib/set.h
@@ -0,0 +1,13 @@
+struct Set {
+    cap:int,
+    len:int,
+    data:int*
+}
+
+fn set_new() -> Set*;
+fn set_free(s:Set*) -> void;
+fn set_size(s:Set*) -> int;
+fn set_contains(s:Set*, x:int) -> int;
+fn set_insert(s:Set*, x:int) -> int;
+fn set_erase(s:Set*, x:int) -> int;
+
